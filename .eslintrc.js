ff5d1707dc049f69ada14f09ac9c6b9df6cf51b7
Abel Lopes <abelflopes@gmail.com>
2023-09-28 12:56:17 +0100

    style: ignore conflicting lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
 /** @type import("eslint").Linter.Config */
 const config = {
   extends: "@abelflopes/eslint-config-tsr-pro",
+  rules: {
+    "unicorn/prevent-abbreviations": 1,
+    "unicorn/prefer-module": 1,
+    "no-template-curly-in-string": 1,
+  },
 };
 
 module.exports = config;
